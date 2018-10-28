417f0969966a7ae50c1977766db33faaa1e5ec60
cubix2ray <cubix2ray@gmail.com>
2018-10-28 17:02:54 +0100

    Models' update

--- website/models.py
+++ website/models.py
@@ -3,15 +3,24 @@ from django.contrib.auth.models import User
 
 
 class Doctor(User):
-    """Let's assume a username is an id number."""
-    gender = models.BooleanField()  # Mr./Ms.
-    title = models.CharField(max_length=50)  # TODO enum
+    """Let's assume a username is a doctor's number needed for receipt."""
+    TITLES = (
+        ('LE', 'lek.'),
+        ('DR', 'dr n. med.'),
+        ('DH', 'dr hab. n. med.'),
+        ('PR', 'prof. dr hab. n. med.'),
+    )
     id = models.PositiveIntegerField(primary_key=True)
+    gender = models.BooleanField()  # true for Mr./false for Ms.
+    title = models.CharField(max_length=2, choices=TITLES, default=TITLES[0][0])
+
+    def __str__(self):
+        return self.title + ' ' + self.first_name + ' ' + self.last_name
 
 
 class Patient(User):
     """Let's assume a username is a pesel number."""
-    gender = models.BooleanField()
+    gender = models.BooleanField() # true for man
     pesel = models.PositiveIntegerField(max_length=11, primary_key=True)
     date_of_birth = models.DateField()
     conditions = models.ManyToManyField('Condition')
@@ -21,11 +30,17 @@ class Patient(User):
             models.Index(fields=['first_name', 'last_name'])
         ]
 
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
 
 class Condition(models.Model):
     """A condition of a patient making him eligible for a refund."""
     id = models.PositiveIntegerField(primary_key=True)
     name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
 
 
 class StoryEntry(models.Model):
@@ -34,19 +49,22 @@ class StoryEntry(models.Model):
     patient = models.ForeignKey('Patient', on_delete=models.CASCADE)
     doctor = models.ForeignKey('Doctor', on_delete=models.SET_NULL)
     date = models.DateTimeField(auto_now_add=True)
-    story = models.ForeignKey('Story', on_delete=models.CASCADE)
+    #story = models.ForeignKey('Story', on_delete=models.CASCADE) no 'Story' model. Is it necessary?
     disease = models.ForeignKey('Disease', on_delete=models.SET_NULL)
     prescription = models.ForeignKey('Prescription', null=True, on_delete=models.SET_NULL)
 
 
 class Disease(models.Model):
+    """Disease coded with ICD 10"""
     code = models.CharField(max_length=5, primary_key=True)
     name = models.CharField(max_length=150)
 
     class Meta:
         indexes = [
             models.Index(fields=['name'])
-        ]
+        ]  
+    def __str__(self):
+        return self.name
 
 
 class Prescription(models.Model):
@@ -57,18 +75,56 @@ class Prescription(models.Model):
 class PrescriptionEntry(models.Model):
     prescription = models.ForeignKey('Prescription', on_delete=models.CASCADE)
     medication = models.ForeignKey('Medication', on_delete=models.SET_NULL)
-    size_of_dose = models.PositiveIntegerField()
-    unit = models.CharField(max_length=30)  # TODO abbreviation, enum?
+    dosage = models.ForeignKey('Dosage', on_delete=models.SET_NULL)
+
+
+class Dosage(models.Model):
+    TIME_OF_DAY = (
+        ('NO', ''),
+        ('RA', ' rano'),
+        ('WI', ' wieczorem'),
+    )
+    EATING = (
+        ('NO', ''),
+        ('NC', ' na czczo'),
+        ('PJ', ' po jedzeniu'),
+    )
+    unit = models.CharField(max_length=30)
     doses_per_day = models.SmallIntegerField()
-    days_between_doses = models.SmallIntegerField()  # 0 if daily doses
+    days_between_doses = models.SmallIntegerField(default=0) # 0 if daily
+    time_of_day = models.CharField(max_length=2, choices=TIME_OF_DAY, default=TIME_OF_DAY[0][0])
+    eating_relation = models.CharField(max_length=2, choices=EATING, default=EATING[0][0])
+
+    def __str__(self):
+        dosage = self.unit + ' '
+        if self.doses_per_day == 1:
+            dosage += '1 raz'
+        else:
+            dosage += str(self.doses_per_day) + ' razy'
+        if self.days_between_doses == 0:
+            dosage += ' dziennie'
+        else:
+            dosage += 'co ' + str(self.days_between_doses) + ' dni'
+        return dosage + self.time_of_day + self.eating_relation
 
 
 class Medication(models.Model):
+    FORM = (
+        ('TB', 'tabletki'),
+        ('ZS', 'zastrzyki'),
+    )
     id = models.PositiveIntegerField(primary_key=True)
     name = models.CharField(max_length=150)
-    active_substance = models.CharField(max_length=150)  # TODO substance model
-    form = models.CharField(max_length=150)  # TODO enum
+    active_substance = models.ManyToManyField('Substance')
+    form = models.CharField(max_length=2, choices=FORM)
     amount = models.PositiveIntegerField()
-    unit = models.CharField(max_length=30)  # TODO unit abbreviation, enum?
+    mg_of_active_substance = models.PositiveIntegerField()
+
+
+class Substance(models.Model):
+    """Active substance checked when checking interactions"""
+    id = models.PositiveIntegerField(primary_key=True)
+    name = models.CharField(max_length=150)
+
 
 #  TODO institutions model
