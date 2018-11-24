a4371d31105a10c2e750331f6353d128453e7c16
cubix2ray <cubix2ray@gmail.com>
2018-11-25 00:26:18 +0100

    Merge migrations

--- website/models.py
+++ website/models.py
@@ -12,7 +12,7 @@ class Patient(User):
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     pesel = models.PositiveIntegerField(primary_key=True)
     date_of_birth = models.DateField()
-    conditions = models.ManyToManyField('Condition', null=True)
+    conditions = models.ManyToManyField('Condition', blank=True)
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
